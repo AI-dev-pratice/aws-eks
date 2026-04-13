0cf4aa95175f8254a1fa992bba5c10cea1f7fbc0
Krishna <krishnads290#gmail.com>
2026-04-13 11:18:56 +0530

    add flux configuration

--- providers.tf
+++ providers.tf
@@ -24,7 +24,6 @@ data "aws_eks_cluster_auth" "cluster" {
 }
 
 provider "helm" {
-  
   kubernetes {
     host                   = aws_eks_cluster.eks-cluster.endpoint
     cluster_ca_certificate = base64decode(aws_eks_cluster.eks-cluster.certificate_authority[0].data)
